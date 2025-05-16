fe7f24d4e09e5d161fea4dadbf1b87232a51f960
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2025-05-16 15:46:09 -0500

    create_home_view

---
+++ src/views/Home-item.vue
@@ -0,0 +1,38 @@
+<script setup>
+import navbar from '@/components/Navbar-item.vue';
+import Footer from '@/components/Footer-item.vue';
+</script>
+
+<template>
+  <header>
+    <navbar />
+  </header>
+  <div class="home-area">
+    <img class="home-image" src="@/assets/homeimg.png" />
+  </div>
+  <footer>
+    <Footer />
+  </footer>
+</template>
+
+<style>
+.home-area {
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+  .home-image {
+    height: 500px;
+  }
+}
+
+@media (min-width: 300px) and (max-width: 768px) {
+  .home-image {
+    height: 400px;
+  }
+}
+</style>
